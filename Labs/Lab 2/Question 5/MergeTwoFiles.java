c9770df7b4b13ae5c0d89174c1d4369c98e8962c
ShandipThapa34 <75742474+ShandipThapa34@users.noreply.github.com>
2022-01-05 22:45:10 +0545

    Add files via upload

---
+++ Labs/Lab 2/Question 5/MergeTwoFiles.java
@@ -0,0 +1,34 @@
+import java.io.*;
+  
+public class MergeTwoFile 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        PrintWriter pw = new PrintWriter("file3.txt");
+        
+        BufferedReader br = new BufferedReader(new FileReader("file1.txt"));
+          
+        String line = br.readLine();
+        while (line != null)
+        {
+            pw.println(line);
+            line = br.readLine();
+        }
+          
+        br = new BufferedReader(new FileReader("file2.txt"));
+          
+        line = br.readLine();
+          
+        while(line != null)
+        {
+            pw.println(line);
+            line = br.readLine();
+        }
+          
+        pw.flush();
+        br.close();
+        pw.close();
+          
+        System.out.println("The first two files have been merged into the third file successfully.");
+    }
+}
