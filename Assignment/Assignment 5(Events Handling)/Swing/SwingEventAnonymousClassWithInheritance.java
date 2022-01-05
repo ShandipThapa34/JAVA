7f3778c3f8be96f4920bfbb5bd3f2fd2f418156b
ShandipThapa34 <75742474+ShandipThapa34@users.noreply.github.com>
2022-01-05 20:01:31 +0545

    Add files via upload

---
+++ Assignment/Assignment 5(Events Handling)/Swing/SwingEventAnonymousClassWithInheritance.java
@@ -0,0 +1,96 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+class SwingEventAnonymousClassWithInheritance extends JFrame
+{
+	JTextField textField1;
+	JTextField textField2;
+	JTextField textField3;
+	JButton button1;
+	JButton button2;
+	JButton button3;
+	JButton button4;
+	SwingEventAnonymousClassWithInheritance()
+	{
+		JLabel label1 = new JLabel("Number 1: ");
+		JLabel label2 = new JLabel("Number 2: ");
+		JLabel label3 = new JLabel("Result  : ");
+		textField1 = new JTextField();
+		textField2 = new JTextField();
+		textField3 = new JTextField();
+		button1 = new JButton("ADD");
+		button2 = new JButton("SUB");
+		button3 = new JButton("Mul");
+		button4 = new JButton("DIV");
+		
+		label1.setBounds(100,100,100,30);
+		label2.setBounds(100,200,100,30);
+		label3.setBounds(100,300,100,30);
+		
+		textField1.setBounds(180,100,200,30);
+		textField2.setBounds(180,200,200,30);
+		textField3.setBounds(180,300,200,30);
+		
+		button1.setBounds(100,400,80,30);
+		button2.setBounds(200,400,80,30);
+		button3.setBounds(300,400,80,30);
+		button4.setBounds(400,400,80,30);
+		
+		add(label1);
+		add(label2);
+		add(label3);
+
+		add(textField2);
+		add(textField3);
+
+		add(button1);
+		add(button2);
+		add(button3);
+		add(button4);
+		
+		setSize(700,600);
+		setTitle("Simple Calculator");
+		setLayout(null);
+		setVisible(true);
+
+		button1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				double numberOne = Double.parseDouble(textField1.getText());
+				double numberTwo = Double.parseDouble(textField2.getText());
+				textField3.setText(String.valueOf(numberOne + numberTwo));
+			}
+		});
+		button2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				double numberOne = Double.parseDouble(textField1.getText());
+				double numberTwo = Double.parseDouble(textField2.getText());
+				textField3.setText(String.valueOf(numberOne - numberTwo));
+			}
+		});
+		button3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				double numberOne = Double.parseDouble(textField1.getText());
+				double numberTwo = Double.parseDouble(textField2.getText());
+				textField3.setText(String.valueOf(numberOne * numberTwo));
+			}
+		});
+		button4.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				double numberOne = Double.parseDouble(textField1.getText());
+				double numberTwo = Double.parseDouble(textField2.getText());
+				textField3.setText(String.valueOf(numberOne / numberTwo));
+			}
+		});
+	}
+	public static void main(String[] args)
+	{
+		new SwingEventAnonymousClassWithAssociation();
+	}
+}
